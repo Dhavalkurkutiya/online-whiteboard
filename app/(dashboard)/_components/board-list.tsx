78283f82612ff5ae7db216701d3bf343ecbb3a6f
Dhaval Kurkutiya <kurkutiyadhaval30@gmail.com>
2024-02-10 16:22:05 +0530

    Board List

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { EmptySearch } from "./empty-search";
-import { EmptyFavorites } from "./empty-favorites";
+
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+
 import { EmptyBoards } from "./empty-boards";
+import { EmptyFavorites } from "./empty-favorites";
+import { EmptySearch } from "./empty-search";
+import { BoardCard } from "./board-card";
+import { title } from "process";
+import { NewBoardButton } from "./new-board-button";
 
 
 interface BoardListProps {
-    orgId?: string;
+    orgId: string;
     query: {
         search?: string;
         favorites?: string;
@@ -16,9 +23,27 @@ interface BoardListProps {
 export const BoardList = ({
     orgId,
     query,
-} : BoardListProps) => {
+}: BoardListProps) => {
 
-    const data = []; //Change to api call
+    const data = useQuery(api.boards.get, { orgId });
+
+    if (data === undefined) {
+        return (
+            <div>
+                <h2 className="text-3xl">
+                    {query.favorites ? "Favorite boards" : "Team Boards"}
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-colos-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+               <NewBoardButton orgId={orgId} disabled />
+               <BoardCard.Skeleton />
+               <BoardCard.Skeleton />
+               <BoardCard.Skeleton />
+               <BoardCard.Skeleton />
+
+                </div>
+            </div>
+        )
+    }
 
     if (!data?.length && query.search) {
         return <EmptySearch />;
@@ -35,7 +60,27 @@ export const BoardList = ({
 
     return (
         <div>
-            {JSON.stringify(query)}
+            <h2 className="text-3xl">
+                {query.favorites ? "Favorite boards" : "Team Boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-colos-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+
+                <NewBoardButton orgId={orgId} />
+
+                {data.map((board) => (
+                    <BoardCard
+                        key={board._id}
+                        id={board._id}
+                        title={board.title}
+                        imageUrl={board.imageUrl}
+                        authorId={board.authorId}
+                        authorName={board.authornName}
+                        createdAt={board._creationTime}
+                        orgId={board.orgId}
+                        isFavorite={false}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
--- convex/boards.ts
+++ convex/boards.ts
@@ -1,40 +1,25 @@
-import  { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { v } from "convex/values"
 
+import { query  } from "./_generated/server"
 
-const images = [
-    "/paceholders/1.svg",
-    "/paceholders/2.svg",
-    "/paceholders/3.svg",
-    "/paceholders/4.svg",
-    "/paceholders/5.svg",
-    "/paceholders/6.svg",
-    "/paceholders/7.svg",
-    "/paceholders/8.svg",
-    "/paceholders/9.svg",
-    "/paceholders/10.svg",
-]
-
-export const create = mutation({
+export const get = query({
     args: {
         orgId: v.string(),
-        title: v.string(),
     },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
+    handler: async(ctx, args) => {
+        const  identity = await ctx.auth.getUserIdentity();
 
-        if(!identity) {
+        if(!identity){
             throw new Error("Unauthorized");
         }
 
-        const randomImage =  images[Math.floor(Math.random() * images.length)];
-        const board  = await ctx.db.insert("boards", {
-            title: args.title,
-            orgId: args.orgId,
-            authorId: identity.subject,
-            authornName: identity.name!,
-            imageUrl: randomImage,
-        })
-        return board;
-    },
-});
+        const boards = await ctx.db
+        .query("boards")
+        .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+        .order("desc")
+        .collect();
+
+        return boards;
+
+    }
+})
