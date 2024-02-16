aa1c5c1d778422d9cf234ac47e90154860d2d083
Dhaval Kurkutiya <kurkutiyadhaval30@gmail.com>
2024-02-16 12:30:45 +0530

    Search Querying

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -25,7 +25,10 @@ export const BoardList = ({
     query,
 }: BoardListProps) => {
 
-    const data = useQuery(api.boards.get, { orgId });
+    const data = useQuery(api.boards.get, { 
+        orgId, 
+        ...query,
+    });
 
     if (data === undefined) {
         return (
--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -1,49 +1,51 @@
 "use client";
 
+import { toast } from "sonner";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
-import { cn } from "@/lib/utils";
-import { useMutation } from "convex/react";
-import { Plus } from "lucide-react";
-import { toast } from "sonner";
 
 interface NewBoardButtonProps {
-    orgId: string;
-    disabled?: boolean;
-}
+  orgId: string;
+  disabled?: boolean;
+};
 
 export const NewBoardButton = ({
-    orgId,
-    disabled = false,
+  orgId,
+  disabled,
 }: NewBoardButtonProps) => {
-    
-    const { mutate, pending } = useApiMutation(api.board.create);
+  const router = useRouter();
+  const { mutate, pending } = useApiMutation(api.board.create);
 
-    const oneClick = () => {
-        if (!pending && !disabled) {
-            mutate({
-                orgId,
-                title: "Untitled",
-            })
-            .then((id) => {
-                toast.success("Board created");
-            })
-            .catch(() => toast.error("Failed to create board"));
-        }
-    };
+  const onClick = () => {
+    mutate({
+      orgId,
+      title: "Untitled"
+    })
+      .then((id) => {
+        toast.success("Board created");
+        router.push(`/board/${id}`);
+      })
+      .catch(() => toast.error("Failed to create board"));
+  }
 
-    return (
-        <button
-            disabled={pending || disabled}
-            onClick={oneClick}
-            className={cn(
-                "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
-                (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
-            )}
-        >
-            <div />
-            <Plus className="h-12 w-12 text-white stroke-1" />
-            <p className="text-sm text-white font-light">New Board</p>
-        </button>
-    );
+  return (
+    <button
+      disabled={pending || disabled}
+      onClick={onClick}
+      className={cn(
+        "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
+        (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
+      )}
+    >
+      <div />
+      <Plus className="h-12 w-12 text-white stroke-1" />
+      <p className="text-sm text-white font-light">
+        New board
+      </p>
+    </button>
+  );
 };
