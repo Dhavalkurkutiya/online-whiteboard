7ea45b7497f1ceb0c09055d1c529c94457a06553
Dhaval Kurkutiya <kurkutiyadhaval30@gmail.com>
2024-02-20 17:24:50 +0530

    Cursors Presence

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,12 +1,19 @@
 "use client"
 
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 
-import { useCanRedo, useCanUndo, useHistory, useSelf } from "@/liveblocks.config"
-import { CanvasMode, CanvasState } from "@/types/canvas"
+import {
+    useCanRedo,
+    useCanUndo,
+    useHistory,
+    useMutation,
+} from "@/liveblocks.config"
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas"
+import { CursorsPresence } from "./cursors-presence"
+import { pointerEventToCanvasPoint } from "@/lib/utils"
 interface ConvasProps {
     boardId: string;
 
@@ -14,15 +21,39 @@ interface ConvasProps {
 
 export const Canvas = ({
     boardId,
-} :ConvasProps) => {
+}: ConvasProps) => {
 
     const [canvasState, setCanvasState] = useState<CanvasState>({
         mode: CanvasMode.None,
-      });
+    });
 
-      const history = useHistory();
-      const canUndo = useCanUndo();
-      const canRedo = useCanRedo();
+    const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 })
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
+
+    const onWheel = useCallback((e: React.WheelEvent) => {
+
+
+        setCamera((camera) => ({
+            x: camera.x - e.deltaX,
+            y: camera.y - e.deltaY,
+        }));
+    }, []);
+
+    const onPointerMove = useMutation((
+        { setMyPresence },
+        e: React.PointerEvent
+    ) => {
+        e.preventDefault();
+        const current = pointerEventToCanvasPoint(e, camera);
+        setMyPresence({ cursor: current });
+    },
+        []);
+
+    const onPointerLeave = useMutation(({ setMyPresence }) => {
+        setMyPresence({ cursor: null });
+    }, []);
 
     return (
         <main
@@ -30,14 +61,24 @@ export const Canvas = ({
         >
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar 
-            canvasState={canvasState}
-            setCanvasState={setCanvasState}
-            canRedo={canRedo}
-            canUndo={canUndo}
-            undo={history.undo}
-            redo={history.redo}
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
             />
+            <svg
+                className="h-[100vh] w-[100vw]"
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
+                onPointerLeave={onPointerLeave}
+            >
+                <g>
+                    <CursorsPresence />
+                </g>
+            </svg>
         </main>
     )
 }
---
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { memo } from "react";
+
+import { useOthersConnectionIds } from "@/liveblocks.config";
+import { connect } from "http2";
+import { Cursor } from "./cursor";
+
+
+const Cursors = () => {
+    const ids = useOthersConnectionIds();
+
+    return (
+        <>
+        {ids.map((connectionId) => (
+            <Cursor
+            key={connectionId}
+            connectionId={connectionId}
+            />
+        ))}
+        </>
+    )
+
+}
+
+export const CursorsPresence = memo(() => {
+    return (
+        <>
+        {/* TODO: Draft pencil */}
+           <Cursors />
+        </>
+    );
+});
+
+CursorsPresence.displayName = "CursorsPresence";
