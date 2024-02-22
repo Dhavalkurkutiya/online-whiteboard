3bbcf3e9d9ef134e750fed90b6b605e2368f54fc
Dhaval Kurkutiya <kurkutiyadhaval30@gmail.com>
2024-02-22 18:16:32 +0530

    Other Element

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { memo } from "react";
-import {useStorage}  from "@/liveblocks.config";
+
 import { LayerType } from "@/types/canvas";
+import { useStorage } from "@/liveblocks.config";
+
+import { Text } from "./text";
+import { Ellipse } from "./ellipse";
 import { Rectangle } from "./rectangle";
+import { Note } from "./note";
+
 interface LayerPreviewProps {
     id: string;
     onLayerPointerDown: (e: React.PointerEvent, layerId: string) => void;
@@ -24,6 +30,33 @@ export const LayerPreview =memo(({
 
     
    switch (layer.type) {
+    case LayerType.Note:
+      return (
+        <Note
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
+    case LayerType.Text:
+      return (
+        <Text
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
+    case LayerType.Ellipse:
+      return (
+        <Ellipse
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
     case LayerType.Rectangle:
     return(
        <Rectangle
@@ -35,17 +68,8 @@ export const LayerPreview =memo(({
     );
     default:
         console.warn("Unknown layer type", layer);
+        return null;
    }
-   
-   return(
-        <div>
-            <h1>Live Preview</h1>
-        </div>
-    )
-    
-
-
-
 });
 
 LayerPreview.displayName = "LayerPreview";
